e3d5dbccc49f303436be2e7a7a0e94bae6a3e745
Ruthie Borces <ruthieborces@outlook.com>
2014-10-20 22:41:29 +0800

    added webform ajax

--- sites/all/modules/custom/ruthiewaypoints/ruthiewaypoints.js
+++ sites/all/modules/custom/ruthiewaypoints/ruthiewaypoints.js
@@ -2,28 +2,42 @@
   Drupal.behaviors.ruthiewaypoints = {
     attach: function (context, settings) {
 
-      var user = Drupal.settings.ruthiewaypoints.user;
-      var ids_array = $("#menu-bootstrap-custom-menu a").map(function() {
-        return $(this).attr("href");
-      });
+      $(document).ready(function(){
+        
+        var user = Drupal.settings.ruthiewaypoints.user;
+        var ids_array = $("#menu-bootstrap-custom-menu a").map(function() {
+          return $(this).attr("href");
+        });
 
-      for (var i = 0; i < ids_array.length; i++) {
-        var curSection = ids_array[i]; 
-        addWaypoint(i, curSection);
-      }
+        for (var i = 0; i < ids_array.length; i++) {
+          var curSection = ids_array[i]; 
+          addWaypoint(i, curSection);
+        }
 
-      function addWaypoint( n, curSection ){
-        $(curSection).waypoint(function(direction) {
-          console.log(curSection, direction);
+        function addWaypoint( n, curSection ){
+          $(curSection).waypoint(function(direction) {
+            console.log(curSection, direction);
+  /*
+            if (direction === 'down') {
+              $('#menu-bootstrap-custom-menu a[href="' + curSection + '"]').addClass('active').prev().removeClass('active');
+            }
+            else {
+              $('#menu-bootstrap-custom-menu a[href="' + curSection + '"]').addClass('active').next().removeClass('active');
+            }
+  */
 
-          if (direction == 'down') {
-            $( "#menu-bootstrap-custom-menu a[href*='" + curSection + "']" ).addClass("active");
-            $( "#menu-bootstrap-custom-menu a[href!='" + curSection + "']" ).removeClass("active");
+          });    
+        }
+
+        mywindow = $(window);
+        mywindow.scroll(function () {
+          if (mywindow.scrollTop() == 0) {
+            $('#menu-bootstrap-custom-menu a').removeClass('active');
+            $('#menu-bootstrap-custom-menu li:first-child a').addClass('active');
           }
-          
-          
-        });    
-      }
+        });
+
+      });
     
     }
   };
